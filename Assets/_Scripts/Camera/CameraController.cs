44253742084ce30b02f6657e99a3e190c77fcad1
RicardoRdz <r.rodrig.baez@gmail.com>
2024-09-20 14:26:44 -0600

    rail fowlloer update camera horizontal offset

--- Assets/_Scripts/Camera/CameraController.cs
+++ Assets/_Scripts/Camera/CameraController.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 namespace RailShooter.Camera
 {
     public class CameraController : RailFollower
     {
-      
+        [SerializeField] private float verticalOffset;
+
+        protected override void TargetPosition()
+        {
+            base.TargetPosition();
+            offsetCamera += followTarget.up * verticalOffset;
+        }
     }
 }
--- Assets/_Scripts/Shared/RailFollower.cs
+++ Assets/_Scripts/Shared/RailFollower.cs
@@ -4,25 +4,22 @@ namespace RailShooter.Camera
 {
     public class RailFollower : MonoBehaviour
     {
-        [SerializeField] private Transform player;
-        [SerializeField] private Transform followTarget;
+        [SerializeField] protected Transform player;
+        [SerializeField] protected Transform followTarget;
         [SerializeField] private float followDistance = 22f;
         [SerializeField] private float smoothTime = 0.2f;
 
-        Vector3 velocity;
-
+        protected Vector3 offsetCamera;
+        private Vector3 velocity;
         private void Update()
-        {           
-            transform.position = Vector3.SmoothDamp(transform.position, TargetPosition(), ref velocity, smoothTime);
-            transform.rotation = player.rotation;
-        }
-
-        protected virtual Vector3 TargetPosition()
         {
-            Vector3 targetPos = followTarget.position + followTarget.forward * -followDistance;
-
-            return targetPos;
+            TargetPosition();
+            transform.SetPositionAndRotation(Vector3.SmoothDamp(transform.position, offsetCamera, ref velocity, smoothTime), player.rotation);
         }
 
+        protected virtual void TargetPosition()
+        {
+            offsetCamera = followTarget.position + followTarget.forward * -followDistance;
+        }
     }
 }
